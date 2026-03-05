b488a7b4dfeb4046e34044247410a913608e8dfe
Felipe Smania <felipesmania13@gmail.com>
2026-03-04 22:06:30 -0300

    implementa validação da quantidade de caracteres

--- Projeto 06 - Gerador de senha aleatória/assets/js/main.js
+++ Projeto 06 - Gerador de senha aleatória/assets/js/main.js
@@ -4,7 +4,17 @@ const copiarSenha = document.getElementById('copiarSenha');
 let senha = document.getElementById('senha');
 let tamanho = document.getElementById('tamanho');
 
+function validaTamanho () {
+    if (isNaN(parseInt(tamanho.value))) {
+        senha.innerText = 'insira uma quantidade de caracteres';
+        return false;
+    }
+    return true;
+}
+
 gerarSenha.addEventListener('click', function() {
+    if (!validaTamanho()) return;
+
     senha.innerText = '';
     for (let i = 0; i < tamanho.value; i++) {
         let aleatorio = Math.floor(Math.random() * (caracteresSenha.length));
