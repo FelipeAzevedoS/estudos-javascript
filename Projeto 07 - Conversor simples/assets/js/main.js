e785301b87c5b2c568e827debe35bd364df87c11
Felipe Smania <felipesmania13@gmail.com>
2026-03-23 21:10:42 -0300

    implement km input validation

--- Projeto 07 - Conversor simples/assets/js/main.js
+++ Projeto 07 - Conversor simples/assets/js/main.js
@@ -4,6 +4,18 @@ const valueMiles = document.getElementById('valueMiles');
 const valueMeters = document.getElementById('valueMeters');
 const valueCentimeters = document.getElementById('valueCentimeters');
 
+function validateKm() {
+    if(valueKilometers.value == '' || isNaN(Number(valueKilometers.value))) {
+        valueMiles.innerText = `Type a valid number`;
+        valueMeters.innerText = ``;
+        valueCentimeters.innerText = ``;
+        valueMiles.classList.add('error');
+        return false;
+    }
+    valueMiles.classList.remove('error');
+    return true;
+};
+
 function calculateValues () {
     let mi = (valueKilometers.value * 0.621371).toFixed(2);
     let m = (valueKilometers.value * 1000).toFixed(2);
@@ -15,5 +27,6 @@ function calculateValues () {
 };
 
 buttonConvert.addEventListener('click', function() {
+    if(!validateKm()) return;
     calculateValues();
 });
