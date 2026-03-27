de23f0985721387b5bb5e881252626e808a15d07
Felipe Smania <felipesmania13@gmail.com>
2026-03-26 21:56:11 -0300

    init project 09 structure

---
+++ Projeto 09 - Pedra, papel e tesoura/assets/js/main.js
@@ -0,0 +1,10 @@
+const youScore = document.querySelector('#youScore');
+const machineScore = document.querySelector('#machineScore');
+const drawsScore = document.querySelector('#drawsScore');
+const yourChoice = document.querySelector('#yourChoice');
+const machineChoice = document.querySelector('#machineChoice');
+const result = document.querySelector('#result');
+const buttonRock = document.querySelector('#buttonRock');
+const buttonPaper = document.querySelector('#buttonPaper');
+const buttonScissors = document.querySelector('#buttonScissors');
+
