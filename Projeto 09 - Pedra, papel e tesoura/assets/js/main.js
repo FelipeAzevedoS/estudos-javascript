d2aee9a0e46481ce69a46b0e8304fd53a03a3675
Felipe Smania <felipesmania13@gmail.com>
2026-03-27 21:05:44 -0300

    implement player and machine choice display

--- Projeto 09 - Pedra, papel e tesoura/assets/js/main.js
+++ Projeto 09 - Pedra, papel e tesoura/assets/js/main.js
@@ -8,3 +8,40 @@ const buttonRock = document.querySelector('#buttonRock');
 const buttonPaper = document.querySelector('#buttonPaper');
 const buttonScissors = document.querySelector('#buttonScissors');
 
+const machineOptions = ['rock', 'paper', 'scissors'];
+
+function machineOption () {
+    let result = machineOptions[Math.floor(Math.random() * 3 )];
+    if (result === 'rock') {
+    machineChoice.innerText += '🪨';
+    } else if (result === 'paper'){
+        machineChoice.innerText += '📄';
+    } else if (result === 'scissors') {
+        machineChoice.innerText += '✂️';
+    }
+    return result;
+};
+
+buttonRock.addEventListener('click', function() {
+    machineChoice.innerText = 'Machine choice:'
+    yourChoice.innerText = 'Your choice:'
+    const machineResult = machineOption();
+
+    yourChoice.innerText += '🪨';
+});
+
+buttonPaper.addEventListener('click', function() {
+    machineChoice.innerText = 'Machine choice:'
+    yourChoice.innerText = 'Your choice:'
+    const machineResult = machineOption();
+
+    yourChoice.innerText += '📄';
+});
+
+buttonScissors.addEventListener('click', function() {
+    machineChoice.innerText = 'Machine choice:'
+    yourChoice.innerText = 'Your choice:'
+    const machineResult = machineOption();
+
+    yourChoice.innerText += '✂️';
+});
