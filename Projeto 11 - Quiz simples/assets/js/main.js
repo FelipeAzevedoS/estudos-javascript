7130a664cf62ee41f7112f1ae610e72e57c1aee7
Felipe Smania <felipesmania13@gmail.com>
2026-04-08 20:22:22 -0300

    init project 11 structure

---
+++ Projeto 11 - Quiz simples/assets/js/main.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const ans1 = document.querySelector('#ans1');
+const ans2 = document.querySelector('#ans2');
+const ans3 = document.querySelector('#ans2');
+const playAgain = document.querySelector('#playAgain');
