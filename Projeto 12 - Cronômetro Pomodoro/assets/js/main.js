7e9fd542f2f440925c0c37b7ed2fdde0a67bb9f1
Felipe Smania <felipesmania13@gmail.com>
2026-04-19 13:42:26 -0300

    init project 12 structure

---
+++ Projeto 12 - Cronômetro Pomodoro/assets/js/main.js
@@ -0,0 +1,5 @@
+const timeImput = document.querySelector('#timeInput');
+const timer = document.querySelector('#timer');
+const start = document.querySelector('#start');
+const pause = document.querySelector('#pause');
+const reset = document.querySelector('#reset');
