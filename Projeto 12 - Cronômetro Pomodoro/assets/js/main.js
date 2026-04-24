e091b05e78c54046add334409ac483a96878585b
Felipe Smania <felipesmania13@gmail.com>
2026-04-23 21:23:52 -0300

    implement countdown timer with MM:SS display

--- Projeto 12 - Cronômetro Pomodoro/assets/js/main.js
+++ Projeto 12 - Cronômetro Pomodoro/assets/js/main.js
@@ -1,5 +1,37 @@
-const timeImput = document.querySelector('#timeInput');
+const timeInput = document.querySelector('#timeInput');
 const timer = document.querySelector('#timer');
 const start = document.querySelector('#start');
 const pause = document.querySelector('#pause');
 const reset = document.querySelector('#reset');
+const chooseMinutes = document.querySelector('#chooseMinutes');
+
+let currentTime;
+let timerFunctionality;
+        
+function inputToTime() {
+    clearInterval(timerFunctionality);
+    currentTime = Number(timeInput.value) * 60;
+    formatTime();
+    timerFunctionality = setInterval(() => {
+        currentTime--;
+        formatTime();
+        if (currentTime <= 0) {
+            clearInterval(timerFunctionality)
+        };
+    }, 1000);
+
+    timer.classList.remove('hidden');
+    timeInput.classList.add('hidden');
+    chooseMinutes.classList.add('hidden');
+    
+};
+
+function formatTime () {
+    
+    let formatedMinutes = Math.floor(currentTime / 60);
+    let formatedSeconds = currentTime % 60;
+
+    timer.textContent = `${String(formatedMinutes).padStart(2, '0')}:${String(formatedSeconds).padStart(2, '0')}`;
+};
+
+start.addEventListener ('click', inputToTime);
