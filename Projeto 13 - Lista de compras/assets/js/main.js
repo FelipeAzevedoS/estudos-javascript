71993f114b0b0ff5147f65890d55defb5142baed
Felipe Smania <felipesmania13@gmail.com>
2026-05-19 23:05:07 -0300

    implement delete item option

--- Projeto 13 - Lista de compras/assets/js/main.js
+++ Projeto 13 - Lista de compras/assets/js/main.js
@@ -4,10 +4,18 @@ const list = document.querySelector('#list');
 
 function createItem () {
     const newLi = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    deleteButton.textContent = 'Delete';
     newLi.textContent = itemInput.value;
 
     newLi.classList.add('list');
+    deleteButton.classList.add('deleteButton');
+
     list.appendChild(newLi);
+    newLi.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => newLi.remove());
     
     itemInput.value = '';
 }
