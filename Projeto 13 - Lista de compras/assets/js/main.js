bd42e1d4fc40bd3f69de884fe8a4d567887036b3
Felipe Smania <felipesmania13@gmail.com>
2026-05-19 22:06:55 -0300

    add create list item functionality

--- Projeto 13 - Lista de compras/assets/js/main.js
+++ Projeto 13 - Lista de compras/assets/js/main.js
@@ -1,2 +1,15 @@
-const input = document.querySelector('#input');
+const itemInput = document.querySelector('#itemInput');
 const buttonAdd = document.querySelector('#buttonAdd');
+const list = document.querySelector('#list');
+
+function createItem () {
+    const newLi = document.createElement('li');
+    newLi.textContent = itemInput.value;
+
+    newLi.classList.add('list');
+    list.appendChild(newLi);
+    
+    itemInput.value = '';
+}
+
+buttonAdd.addEventListener('click', createItem);
