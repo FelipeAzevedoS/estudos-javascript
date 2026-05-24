85aa79d9e11d4251e9e569ee5e429c85cde4496f
Felipe Smania <felipesmania13@gmail.com>
2026-05-24 13:57:24 -0300

    implement add and delete items in list

--- Projeto 13 - Lista de compras/assets/js/main.js
+++ Projeto 13 - Lista de compras/assets/js/main.js
@@ -1,23 +1,53 @@
 const itemInput = document.querySelector('#itemInput');
 const buttonAdd = document.querySelector('#buttonAdd');
 const list = document.querySelector('#list');
+const itemList = [];
 
-function createItem () {
+const savedList = localStorage.getItem('shoppingList');
+const parsedList = JSON.parse(savedList);
+
+if(parsedList == null) {
+} else{
+    parsedList.forEach(createItem);
+}
+
+function createItem (item) {
+    let itemText;
     const newLi = document.createElement('li');
     const deleteButton = document.createElement('button');
 
     deleteButton.textContent = 'Delete';
-    newLi.textContent = itemInput.value;
-
+    
+    if(typeof item === 'string') {
+        itemText = item;
+    } else {
+        if (!itemInput.value)return;
+        itemText = itemInput.value;
+    }
+    
+    newLi.textContent = itemText;
+    itemList.push(itemText);
     newLi.classList.add('list');
     deleteButton.classList.add('deleteButton');
-
+    
     list.appendChild(newLi);
     newLi.appendChild(deleteButton);
 
-    deleteButton.addEventListener('click', () => newLi.remove());
+    const JSONList = JSON.stringify(itemList);
     
+    deleteButton.addEventListener('click', deleteAndSave);
+
+    function deleteAndSave() {
+        newLi.remove();
+        const itemDeleteNum = itemList.indexOf(itemText);
+        itemList.splice(itemDeleteNum, 1);
+        const JSONList = JSON.stringify(itemList);
+        localStorage.setItem('shoppingList', JSONList);
+    }
+    
+    localStorage.setItem('shoppingList', JSONList);
+
     itemInput.value = '';
 }
 
-buttonAdd.addEventListener('click', createItem);
+buttonAdd.addEventListener('click', () => createItem());
